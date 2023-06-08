491322b2ad7119bf596090348fba0ffacfdee286
myluki2000 <lukas636@live.com>
2023-06-08 17:07:56 +0200

    Remove user service from execution (not done yet)

--- docker compose build.sh
+++ docker compose build.sh
@@ -2,8 +2,8 @@
 # new services need to be added as they get worked on
 # make sure that all projects are in the same folder e.g. gits
 docker compose \
+-f ./graphql_gateway/docker-compose.yml \
 -f ./media_service/docker-compose.yml \
--f ./user_service/docker-compose.yml \
 -f ./course_service/docker-compose.yml \
 -f ./content_service/docker-compose.yml \
 --project-name gits \
---
+++ docker compose up.bat
@@ -0,0 +1,1 @@
+docker compose -f ./graphql_gateway/docker-compose.yml -f ./media_service/docker-compose.yml -f ./course_service/docker-compose.yml -f ./content_service/docker-compose.yml --project-name gits up -d
